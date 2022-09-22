319ecf7db5b8ee3d1e40f4d3784506b389585516
Oswaldo Gomes Moreira <76781013+ozMoreira@users.noreply.github.com>
2022-09-21 23:59:39 -0300

    Subindo testes do bootstrap

---
+++ Fiap.Aula04Refat/Fiap.Aula04Refat.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fiap.Aula04Refat.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
